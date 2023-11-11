aa728f66edb3a12165db49ee1a575169ec2b367d
Sekou S Seh <sehsekou98@gmail.com>
2023-11-11 06:09:36 +0100

    update

--- src/main/java/com/example/demo/service/JBDCStudentService.java
+++ src/main/java/com/example/demo/service/JBDCStudentService.java
@@ -42,31 +42,94 @@ public class JBDCStudentService implements StudentDb {
 
     @Override
     public boolean existsPersonWithIdNumber(long idNumber) {
-        return false;
+        var sql = """
+                SELECT count(id)
+                FROM student
+                WHERE idNumber = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, idNumber);
+        return count != null && count > 0;
     }
 
     @Override
     public void insertStudent(Student student) {
-
+        var sql = """
+                INSERT INTO student(name, email, department, idnumber)
+                VALUES (?, ?, ?, ?)
+                """;
+        int result = jdbcTemplate.update(
+                sql,
+                student.getName(),
+                student.getEmail(),
+                student.getDepartment(),
+                student.getIdNumber()
+        );
+        System.out.println("jdbcTemplate.update = " + result);
     }
 
     @Override
     public boolean existsPersonWithEmail(String email) {
-        return false;
+        var sql = """
+                SELECT count(id)
+                FROM student
+                WHERE email = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
+
+        return count != null && count > 0;
     }
 
     @Override
     public boolean existsPersonWithId(Integer id) {
-        return false;
+        var sql = """
+                SELECT count(id)
+                FROM student
+                WHERE id = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class,id);
+        return count != null && count > 0;
     }
 
     @Override
     public void deleteStudentById(Integer studentId) {
+        var sql = """
+                DELETE
+                FROM student
+                WHERE id = ?
+                """;
+        int result = jdbcTemplate.update(sql, studentId);
+        System.out.println("deleteStudentById result = " + result);
 
     }
 
     @Override
     public void updateStudent(Student update) {
-
+    if (update.getName() != null) {
+        String sql = "UPDATE student SET name = ? WHERE id = ?";
+        int result = jdbcTemplate.update(
+                sql,
+                update.getName(),
+                update.getId()
+        );
+        System.out.println("update student name result = " + result);
+    }
+        if (update.getEmail() != null) {
+            String sql = "UPDATE student SET email = ? WHERE id = ?";
+            int result = jdbcTemplate.update(
+                    sql,
+                    update.getEmail(),
+                    update.getId()
+            );
+            System.out.println("update student email result = " + result);
+        }
+        if (update.getDepartment() != null) {
+            String sql = "UPDATE student SET department = ? WHERE id = ?";
+            int result = jdbcTemplate.update(
+                    sql,
+                    update.getDepartment(),
+                    update.getId()
+            );
+            System.out.println("update student department result = " + result);
+        }
     }
 }
